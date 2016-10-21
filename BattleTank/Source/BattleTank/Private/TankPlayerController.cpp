289c722fff8e43b094a06b2a2535480411fefcd4
X01\Jordan <regnier@mail.com>
2016-10-21 19:45:59 -0400

    BT19 Creating an AI Controller Class

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,6 +4,22 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController posessing %s"), *(ControlledTank->GetName()));
+	}
+		UE_LOG(LogTemp, Warning,TEXT("PlayerController Begin Play"));
+}
+
 
 ATank* ATankPlayerController :: GetControlledTank() const
 {
